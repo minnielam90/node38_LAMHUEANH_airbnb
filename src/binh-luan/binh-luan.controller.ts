2a234cc1d0c70e56279a668b0bef4a63c9040fb3
minnielam90 <minnie.lam90@gmail.com>
2024-02-27 11:22:23 +0700

    cap nhat binh luan

--- src/binh-luan/binh-luan.controller.ts
+++ src/binh-luan/binh-luan.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Res } from '@nestjs/common';
 import { BinhLuanService } from './binh-luan.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateBinhLuanDto } from './dto/create-binh-luan.dto';
+import { UpdateBinhLuanDto } from './dto/update-binh-luan.dto';
 
 @ApiTags('BinhLuan')
 @Controller('binh-luan')
@@ -10,5 +12,32 @@ export class BinhLuanController {
   @Get()
   getBinhLuan(@Res() res): any {
     return this.binhLuanService.fetchBinhLuanApi(res);
+  }
+
+  @Post()
+  createBinhLuan(@Body() body: CreateBinhLuanDto, @Res() res): any {
+    return this.binhLuanService.createBinhLuanApi(body, res);
+  }
+
+  @Put('/:id')
+  updateBinhLuan(
+    @Body() body: UpdateBinhLuanDto,
+    @Param('id') idBinhLuan: number,
+    @Res() res,
+  ): any {
+    return this.binhLuanService.updateBinhLuan(body, idBinhLuan, res);
+  }
+
+  @Delete('/:id')
+  deleteBinhLuan(@Param('id') idBinhLuan: number, @Res() res): any {
+    return this.binhLuanService.deleteBinhLuanApi(idBinhLuan, res);
+  }
+
+  @Get('/lay-binh-luan-theo-phong/:MaPhong')
+  getInfoBinhLuanTheoMaPhong(
+    @Param('MaPhong') maPhong: number,
+    @Res() res,
+  ): any {
+    return this.binhLuanService.getInfoBinhLuanTheoMaPhongApi(maPhong, res);
   }
 }
--- src/binh-luan/binh-luan.service.ts
+++ src/binh-luan/binh-luan.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaClient } from '@prisma/client';
+import { CreateBinhLuanDto } from './dto/create-binh-luan.dto';
+import { UpdateBinhLuanDto } from './dto/update-binh-luan.dto';
 
 @Injectable()
 export class BinhLuanService {
@@ -20,6 +22,156 @@ export class BinhLuanService {
             },
           });
           return res.status(200).send(data);
+        } catch {
+          return res.status(500).send('Lỗi BE!');
+        }
+      }
+
+      async createBinhLuanApi(body: CreateBinhLuanDto, res): Promise<any> {
+        try {
+          let {
+            ma_phong,
+            ma_nguoi_binh_luan,
+            ngay_binh_luan,
+            noi_dung,
+            sao_binh_luan,
+          } = body;
+    
+          let checkMaPhong = await this.prisma.phong.findFirst({
+            where: {
+              id: ma_phong,
+            },
+          });
+    
+          let checkMaNguoiBinhLuan = await this.prisma.nguoi_dung.findFirst({
+            where: {
+              id: ma_nguoi_binh_luan,
+            },
+          });
+    
+          if (checkMaPhong && checkMaNguoiBinhLuan) {
+            let data = {
+              ma_phong,
+              ma_nguoi_binh_luan,
+              ngay_binh_luan,
+              noi_dung,
+              sao_binh_luan,
+            };
+    
+            let newBinhLuan = await this.prisma.binh_luan.create({
+              data: data,
+            });
+            return res.status(201).send(newBinhLuan);
+          } else if (!checkMaPhong) {
+            return res.status(404).send('Mã phòng không tồn tại!');
+          } else if (!checkMaNguoiBinhLuan) {
+            return res.status(404).send('Mã người dùng không tồn tại!');
+          }
+        } catch {
+          return res.status(500).send('Lỗi BE!');
+        }
+      }
+
+      async updateBinhLuan(body: UpdateBinhLuanDto, idBinhLuan, res): Promise<any> {
+        try {
+          let {
+            ma_phong,
+            ma_nguoi_binh_luan,
+            ngay_binh_luan,
+            noi_dung,
+            sao_binh_luan,
+          } = body;
+    
+          let checkId = await this.prisma.binh_luan.findFirst({
+            where: {
+              id: Number(idBinhLuan),
+            },
+          });
+    
+          let checkMaPhong = await this.prisma.phong.findFirst({
+            where: {
+              id: ma_phong,
+            },
+          });
+    
+          let checkMaNguoiBinhLuan = await this.prisma.nguoi_dung.findFirst({
+            where: {
+              id: ma_nguoi_binh_luan,
+            },
+          });
+    
+          if (checkMaPhong && checkMaNguoiBinhLuan && checkId) {
+            let updateData = {
+              id: Number(idBinhLuan),
+              ma_phong,
+              ma_nguoi_binh_luan,
+              ngay_binh_luan,
+              noi_dung,
+              sao_binh_luan,
+            };
+    
+            let updateBinhLuan = await this.prisma.binh_luan.update({
+              where: {
+                id: Number(idBinhLuan),
+              },
+              data: updateData,
+            });
+            return res.status(201).send(updateBinhLuan);
+          } else if (!checkMaPhong) {
+            return res.status(404).send('Mã phòng không tồn tại!');
+          } else if (!checkMaNguoiBinhLuan) {
+            return res.status(404).send('Mã người dùng không tồn tại!');
+          } else if (!checkId) {
+            return res.status(404).send('Mã bình luận không tồn tại!');
+          }
+        } catch {
+          return res.status(500).send('Lỗi BE!');
+        }
+      }
+    
+      async deleteBinhLuanApi(idBinhLuan: number, res): Promise<any> {
+        try {
+          let checkId = await this.prisma.binh_luan.findFirst({
+            where: {
+              id: Number(idBinhLuan),
+            },
+          });
+    
+          if (checkId) {
+            await this.prisma.binh_luan.delete({
+              where: {
+                id: Number(idBinhLuan),
+              },
+            });
+            return res.status(201).send('Xóa bình luận thành công!');
+          } else {
+            return res.status(404).send('Mã bình luận không tồn tại');
+          }
+        } catch {
+          return res.status(500).send('Lỗi BE!');
+        }
+      }
+    
+      async getInfoBinhLuanTheoMaPhongApi(maPhong: number, res): Promise<any> {
+        try {
+          let checkId = await this.prisma.binh_luan.findFirst({
+            where: {
+              id: Number(maPhong),
+            },
+          });
+          if (checkId) {
+            let data = await this.prisma.binh_luan.findMany({
+              where: {
+                ma_phong: Number(maPhong),
+              },
+              include: {
+                nguoi_dung: true,
+              },
+            });
+            return res.status(200).send(data);
+          } else {
+            return res.status(404).send('Mã phòng không tồn tại!');
+          }
         } catch {
           return res.status(500).send('Lỗi BE!');
         }
